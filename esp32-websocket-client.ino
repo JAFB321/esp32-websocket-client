e3221e7e330a9ae558e0ef03f8ace08fd804b0d8
JAFB321 <jafb321@gmail.com>
2021-10-08 18:39:06 -0700

    feat: wifi conection

--- esp32-websocket-client.ino
+++ esp32-websocket-client.ino
@@ -1,15 +1,31 @@
 #include <WebSockets.h>
 #include <WebSocketsClient.h>
-    
+#include <WiFi.h>
+
+ // Wifi Config
+const char* ssid = "INFINITUM_FB";
+const char* pass = "GFEABL1969";
+
+
 WebSocketsClient socket;
 
 void setup()
 {
-    
+    Serial.begin(9600);
+
+    WiFi.begin(ssid, pass);
+
+    Serial.println("Connecting...");
+    while(WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("Connected");
+    Serial.println(WiFi.localIP());
 
 }
 
 void loop()
 {
-    
 }

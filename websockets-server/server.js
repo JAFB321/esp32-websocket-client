280190b47e778fba9f7f678889efacd2ae532dcc
JAFB321 <jafb321@gmail.com>
2021-10-08 20:11:32 -0700

    fix: add logs on websocket events

--- websockets-server/server.js
+++ websockets-server/server.js
@@ -5,7 +5,12 @@ const server = new WebSocketServer({
 });
 
 server.on('connection', (socket) => {
+
+    console.log('Socket connected!!');
+
     socket.on('message', (data) => {
-        console.log('Message received: ', data.tostring());
+        console.log('Message received: ', data.toString());
     });
 });
+
+server.on('listening', () => console.log('Websocket server listen on port: ', server.options.port));
